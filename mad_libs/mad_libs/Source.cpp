b46083e52c7760c70c7a4f7502e7d1a3f5213d18
Jacqueline Nguyen <beuyenj@gmail.com>
2019-03-17 14:03:18 -0700

    Fixed minor formatting errors.

--- mad_libs/mad_libs/Source.cpp
+++ mad_libs/mad_libs/Source.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 	cout << "Let me generate a story for you!\n";
-	string noun1, noun2, noun3, noun4, noun5, noun6, fem, pl_noun1, pl_noun2, verb, person_in_room, body1, body2, adj1, adj2, liquid;
-	cout << "Enter 6 nouns, separated by spaces: ";
-	cin >> noun1 >> noun2 >> noun3 >> noun4 >> noun5 >> noun6;
+	string noun1, noun2, noun3, noun4, noun5, noun6, noun7, fem, pl_noun1, pl_noun2, verb, person_in_room, body1, pl_body, adj1, adj2, liquid;
+	cout << "Enter 7 nouns, separated by spaces: ";
+	cin >> noun1 >> noun2 >> noun3 >> noun4 >> noun5 >> noun6 >> noun7;
 	cout << "Enter a woman's name: ";
 	cin >> fem;
 	cout << "Enter 2 plural nouns, separated by a space: ";
@@ -15,24 +15,26 @@ int main() {
 	cin >> verb;
 	cout << "Enter the name of a person in the room: ";
 	cin >> person_in_room;
-	cout << "Enter 2 body parts, separated by a space: ";
-	cin >> body1 >> body2;
+	cout << "Enter a body part: ";
+	cin >> body1;
+	cout << "Enter a body part that's plural: ";
+	cin >> pl_body;
 	cout << "Enter 2 adjectives, separated by a space: ";
 	cin >> adj1 >> adj2;
 	cout << "Enter the name of a liquid: ";
 	cin >> liquid;
-	cout << "It was Thanksgiving, and the scent of succulent roast "
-		<< noun1 << "wafted through my house. \"" << person_in_room << ", it's my time to "
+	cout << endl << "It was Thanksgiving, and the scent of succulent roast "
+		<< noun1 << " wafted through my house. \"" << person_in_room << ", it's my time to "
 		<< verb << "!\" my mother called. I couldn't wait to get my "
 		<< body1 << " on that " << adj1 << " Thanksgiving meal. My family "
-		<< " sat around the dining room " << noun2 << ". The tabe was laid "
+		<< "sat around the dining room " << noun2 << ". The table was laid "
 		<< "out with every kind of " << noun3 << ". There was a basket of hot "
 		<< "buttered " << pl_noun1 << " and glasses of sparkling " << liquid
 		<< ". The " << adj2 << " turkey sat, steaming, next to a tureen of "
-		<< noun3 << " gravy. A bowl of ruby-red " << noun4 << " sauce, a sweet-"
-		<< noun5 << " casserole, and a dish of mashed " << pl_noun2 << " tempted "
+		<< noun4 << " gravy. A bowl of ruby-red " << noun5 << " sauce, a sweet-"
+		<< noun6 << " casserole, and a dish of mashed " << pl_noun2 << " tempted "
 		<< "my tastebuds. But the dish I looked forward to the most was Grandma "
-		<< fem << "'s famous " << noun6 << " pie. Thanksgiving is my favorite "
-		<< "holiday, " << body2 << " down." << endl;
+		<< fem << "'s famous " << noun7 << " pie. Thanksgiving is my favorite "
+		<< "holiday, " << pl_body << " down." << endl;
 	return 0;
 }
