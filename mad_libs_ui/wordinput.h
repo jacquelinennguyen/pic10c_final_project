8f8ed219211cbc074d4fe062df79926fee72587c
Jacqueline Nguyen <beuyenj@gmail.com>
2019-03-31 19:23:29 -0700

    Stuck on sorting algorithm

---
+++ mad_libs_ui/wordinput.h
@@ -0,0 +1,32 @@
+#ifndef WORDINPUT_H
+#define WORDINPUT_H
+
+#include <QDialog>
+#include <QAbstractButton>
+#include "story.h"
+#include "madlibs.h"
+
+namespace Ui {
+class WordInput;
+}
+
+class WordInput : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit WordInput(QWidget *parent = nullptr);
+    ~WordInput();
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_buttonBox_rejected();
+
+private:
+    Ui::WordInput *ui;
+    //Story *story;
+    //MadLibs *mainW;
+};
+
+#endif // WORDINPUT_H
